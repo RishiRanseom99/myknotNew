096e586ec616202d853d95097567a63e5e6f8659
unknown <rishisinghal1999@gmail.com>
2020-12-30 11:38:27 +0530

    made form backend

--- main.js
+++ main.js
@@ -34,3 +34,59 @@ const addMoreFAQ=()=>{
     
 
 };
+
+
+document.getElementById('form__submit').addEventListener('click',function(event){
+    event.preventDefault();
+    console.log("inside function");
+    const name=document.getElementById('name').value;
+    const email=document.getElementById('email').value;
+    const phoneno=document.getElementById('phoneno').value;
+    const company=document.getElementById('company').value;
+    const service=document.getElementById('service').value;
+    const date=new Date();
+    // validation start
+let warnings="Please fill out ";
+const WarningOBJ=document.getElementById('form__warnings');
+    if(name.length==0)
+    {
+        warnings+="Name, ";
+    }
+    if(email.length==0)
+    {
+        warnings+="Email ";
+    }
+    else if(!/[0-9A-Za-z][0-9A-Za-z]*@[A-Za-z][A-za-z]*.[a-z][a-z]*$/.test(email))
+   {
+       warnings+="Correct Email ";
+   }
+    console.log(warnings);
+    // validation end
+
+    if(warnings!="Please fill out ")
+    {WarningOBJ.innerHTML=warnings;}
+    else{
+        console.log(name,email,phoneno,company,service,date);
+    fetch("https://api.apispreadsheets.com/data/5758/", {
+        method: "POST",
+        body: JSON.stringify({"data": {"Name":name,"Email":email,"Phone No":phoneno,"Company Name":company,"Type of Service":service,"DATE":date}}),
+    }).then(res =>{
+        if (res.status === 201){
+            // SUCCESS
+            WarningOBJ.innerHTML="We Will contact you soon";
+            
+        }
+        else{
+            // ERROR
+            WarningOBJ.innerHTML="Please refill the form";
+            
+        }
+        setTimeout(()=>{
+            WarningOBJ.innerHTML="";
+        },5000);
+    })
+    }
+    
+    
+    
+});
